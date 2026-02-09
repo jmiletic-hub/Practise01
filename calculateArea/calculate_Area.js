f41a3b25c1b85d76360df787ed4eeb2c49555100
Jelena <j.miletic@logifuture.com>
2026-02-09 14:07:11 +0100

    messsage

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,10 @@
+let length;
+let width;
+function calculateArea() {
+
+}
+function calculateArea() {
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
+}
+ document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
