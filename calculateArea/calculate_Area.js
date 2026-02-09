3f2ef3c523b50fbab2824a1df90bd53dd417e0a0
Jelena <j.miletic@logifuture.com>
2026-02-09 16:42:12 +0100

    Practice task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,24 @@
 let length;
 let width;
-function calculateArea() {
 
-}
 function calculateArea() {
  length = parseFloat(document.getElementById('length').value);
  width = parseFloat(document.getElementById('width').value);
+
+let area = length * width;
+ document.getElementById('result').innerText = `The ara of the rectangle is: ${area}`;
+
 }
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+
+ function calculateSum (){
+    const amount1Element = document.getElementById("amount1");
+    console.log(amount1Element);
+    console.log(amount1Element.value);
+    console.log(typeof amount1Element.value)
+    
+    let amount1= parseFloat (document.getElementById("amount1").value);
+    let amount2= parseFloat(document.getElementById("amount2").value);
+    let amount3= parseFloat(document.getElementById("amount3").value);
+    let sum= amount1+amount2+amount3;
+    document.getElementById("sumResult").innerText="Sum is " + sum;
+ }
